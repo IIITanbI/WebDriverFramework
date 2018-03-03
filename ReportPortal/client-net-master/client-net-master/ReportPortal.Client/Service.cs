395734519d04b068b4217e08faeb5c766772e13f
artyom.kuis <artyom.kuis@gmail.com>
2018-03-03 16:20:36 +0300

    fix

--- ReportPortal/client-net-master/client-net-master/ReportPortal.Client/Service.cs
+++ ReportPortal/client-net-master/client-net-master/ReportPortal.Client/Service.cs
@@ -1,17 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using ReportPortal.Client.Extentions;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
 
 namespace ReportPortal.Client
 {
+    public class DriverObjectProxy : RealProxy
+    {
+        private MyClient client;
+
+        public DriverObjectProxy(HttpMessageHandler handler) : base(typeof(IMyClient))
+        {
+            client = new MyClient(handler);
+            this.Handler = handler;
+        }
+        public DriverObjectProxy(HttpMessageHandler handler, bool disposeHandler) : base(typeof(IMyClient))
+        {
+            client = new MyClient(handler, disposeHandler);
+            this.Handler = handler;
+            this.DisposeHandler = disposeHandler;
+        }
+
+        public HttpMessageHandler Handler { get; set; }
+
+        public bool DisposeHandler { get; set; }
+
+
+        protected static ReturnMessage InvokeMethod(object representedValue, IMethodCallMessage msg)
+        {
+            if (msg == null)
+            {
+                throw new ArgumentNullException(nameof(msg), "The message containing invocation information cannot be null");
+            }
+
+            MethodInfo proxiedMethod = msg.MethodBase as MethodInfo;
+            var result = proxiedMethod.Invoke(representedValue, msg.Args);
+            return new ReturnMessage(result, null, 0, msg.LogicalCallContext, msg);
+        }
+
+        public override IMessage Invoke(IMessage msg)
+        {
+            try
+            {
+                IMethodCallMessage methodCallMessage = msg as IMethodCallMessage;
+                Console.WriteLine(methodCallMessage.MethodName);
+                return InvokeMethod(client, methodCallMessage);
+            }
+            catch (Exception ex)
+            {
+                lock (Console.Out)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.ToString());
+                    Console.ResetColor();
+                }
+
+                throw;
+            }
+        }
+    }
+
+    public class MyClient : HttpClient, IMyClient
+    {
+        public MyClient(HttpMessageHandler handler) : base(handler)
+        {
+        }
+
+        public MyClient(HttpMessageHandler handler, bool disposeHandler) : base(handler, disposeHandler)
+        {
+        }
+    }
     /// <summary>
     /// Class to interact with common Report Portal services. Provides possibility to manage almost of service's entities.
     /// </summary>
     public partial class Service
     {
-        private readonly HttpClient _httpClient;
+        private readonly IMyClient _httpClient;
         private readonly HttpClientHandler _httpHandler;
 
         /// <summary>
@@ -24,7 +94,7 @@ namespace ReportPortal.Client
         {
             _httpHandler = new HttpClientHandler();
 
-            _httpClient = new HttpClient(_httpHandler);
+            _httpClient = (IMyClient)(new DriverObjectProxy(_httpHandler).GetTransparentProxy());
             _httpClient.BaseAddress = uri;
 
             _httpClient.DefaultRequestHeaders.Clear();
