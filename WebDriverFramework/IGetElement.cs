87945e4114cabe701d9ecdc3d0d4f541bfdb2753
artyom.kuis <artyom.kuis@gmail.com>
2018-03-14 14:57:12 +0300

    refactor

--- WebDriverFramework/IGetElement.cs
+++ WebDriverFramework/IGetElement.cs
@@ -1,16 +1,9 @@
 ﻿namespace WebDriverFramework
 {
     using OpenQA.Selenium;
-    using System.Collections.Generic;
 
     public interface IGetElement
     {
         T Get<T>(By locator);
-        //IEnumerable<T> GetAll<T>(By locator);
-    }
-
-    public interface IGetElements
-    {
-        IEnumerable<T> GetAll<T>(By locator);
     }
 }
---
+++ WebDriverFramework/IGetElements.cs
@@ -0,0 +1,10 @@
+﻿namespace WebDriverFramework
+{
+    using OpenQA.Selenium;
+    using System.Collections.Generic;
+
+    public interface IGetElements
+    {
+        IEnumerable<T> GetAll<T>(By locator);
+    }
+}
